9fb29ac6c303da99be5156c34a9603fdbe3ffa34
FrancoLab <francolabuschagne90@gmail.com>
2024-04-05 09:07:30 +0200

    Update ESLint and Prettier configurations

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import nocache from "nocache";
 dotenv.config();
 
 if (!(process.env.PORT && process.env.CLIENT_ORIGIN_URL)) {
-  throw new Error("Missing required environment variables");
+    throw new Error("Missing required environment variables");
 }
 
 const PORT = parseInt(process.env.PORT, 10);
@@ -25,21 +25,21 @@ app.set("json spaces", 2);
 
 // Security headers
 app.use(
-  helmet({
-    hsts: {
-      maxAge: 31536000,
-    },
-    contentSecurityPolicy: {
-      useDefaults: false,
-      directives: {
-        "default-src": ["'none'"],
-        "frame-ancestors": ["'none'"],
-      },
-    },
-    frameguard: {
-      action: "deny",
-    },
-  }),
+    helmet({
+        hsts: {
+            maxAge: 31536000,
+        },
+        contentSecurityPolicy: {
+            useDefaults: false,
+            directives: {
+                "default-src": ["'none'"],
+                "frame-ancestors": ["'none'"],
+            },
+        },
+        frameguard: {
+            action: "deny",
+        },
+    }),
 );
 
 // Logger
@@ -50,19 +50,19 @@ app.use(nocache());
 
 // Cors settings
 app.use(
-  cors({
-    origin: CLIENT_ORIGIN_URL,
-    methods: ["GET"],
-    allowedHeaders: ["Authorization", "Content-Type"],
-    maxAge: 86400,
-  }),
+    cors({
+        origin: CLIENT_ORIGIN_URL,
+        methods: ["GET"],
+        allowedHeaders: ["Authorization", "Content-Type"],
+        maxAge: 86400,
+    }),
 );
 
 // Health Check
 app.get("/", (req, res) => {
-  res.json({
-    message: "🎉 Express is running and healthy",
-  });
+    res.json({
+        message: "🎉 Express is running and healthy",
+    });
 });
 
 /**
@@ -77,19 +77,19 @@ app.get("/", (req, res) => {
 
 // Guard route
 app.use((req, res, next) => {
-  const error = {
-    status: 404,
-    message: "Route not found",
-  };
+    const error = {
+        status: 404,
+        message: "Route not found",
+    };
 
-  next(error);
+    next(error);
 });
 
 // Server starter - You can use this to start any other services that needs to be running before the server starts
 async function startServer() {
-  app.listen(PORT, () => {
-    console.log(`🚀 Server listening on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`🚀 Server listening on port ${PORT}`);
+    });
 }
 
 startServer();
