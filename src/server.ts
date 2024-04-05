793a2eea581a4d759df7358e6dabf49aa4057c6f
FrancoLab <francolabuschagne90@gmail.com>
2024-04-05 09:24:57 +0200

    Add support for additional HTTP methods in server.ts

--- src/server.ts
+++ src/server.ts
@@ -52,7 +52,7 @@ app.use(nocache());
 app.use(
     cors({
         origin: CLIENT_ORIGIN_URL,
-        methods: ["GET"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Authorization", "Content-Type"],
         maxAge: 86400,
     }),
